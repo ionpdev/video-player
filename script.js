a5c6bb5a360b2f89b5e1686d783bfd29ecf35bcf
johny2433 <johny.codmw3@gmail.com>
2020-11-13 15:09:37 +0200

    play and pause video functionality

--- script.js
+++ script.js
@@ -10,13 +10,26 @@ const duration = document.querySelector(".time-duration");
 const fullscreenBtn = document.querySelector(".fullscreen");
 
 // Play & Pause ------------------------------//
+
+function showPlayIcon() {
+  playBtn.classList.replace("fa-pause", "fa-play");
+  playBtn.setAttribute("title", "Play");
+}
+
 function togglePlay() {
   if (video.paused) {
     video.play();
+    playBtn.classList.replace("fa-play", "fa-pause");
+    playBtn.setAttribute("title", "Pause");
   } else {
     video.pause();
+    showPlayIcon();
   }
 }
+// On Video End, show the play button Icon
+video.addEventListener("ended", showPlayIcon);
+// -----
+
 // Progress Bar ------------------------------ //
 
 // Volume Controls --------------------------- //
